f720557fe1de37154bc92433ce941a9e63e90f0b
Maciej Ludwik Kaczkowski <m.kaczkowski@stud.elka.pw.edu.pl>
2021-03-31 19:26:51 +0200

    working on board.get_moves

--- board.py
+++ board.py
@@ -11,13 +11,14 @@ from config import WHITE, BLACK
 class Board:
 
     def __init__(self, board_size):
-        self.board = np.zeros(board_size, board_size)
+        self.board = np.zeros((board_size, board_size), dtype=int)
         self.board[3][4] = BLACK
         self.board[4][3] = BLACK
         self.board[3][3] = WHITE
         self.board[4][4] = WHITE
         self.possible_moves = np.array(1)
         self.playing_next = BLACK
+        self.board_size = board_size
 
     def get_moves(self) -> 'get list of possible moves':
         if self.playing_next == BLACK:
@@ -28,8 +29,20 @@ class Board:
         while not player_stones.size == 0:
             current_stone = player_stones[-1]
             # TODO: check all 8 directions, finish get_moves
+            self.check_directions(current_stone[0], current_stone[1])
             np.append(self.possible_moves, )
             np.delete(player_stones, -1)
+
+    def check_directions(self, x, y) -> 'checks all 8 directions for get_moves method':
+        colour = self.board[x][y]
+        # print(colour)
+        # oposing_neighbours = [[self.board[i][j] if i <= self.board_size and j <= self.board_size and self.board[i][j] == -colour else 9
+        #                        for i in range(point[0] - 1, point[0] + 1)]
+        #                       for j in range(point[1] - 1, point[1] + 1)]
+
+        # return oposing_neighbours
+
+        pass
 
     def heuristic_evaluate(self):
         game_state = self.board
